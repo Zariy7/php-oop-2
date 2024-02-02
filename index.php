1848367cb168b1078f76490f38fe03f4dbb979ca
Zariy7 <nazariy.hereha@gmail.com>
2024-02-02 08:43:54 +0100

    data declarations done

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
         public $species;
 
         function __construct(String $_name, Float $_price, String $_image, Species $_species){
-            $this->$name = $_name;
-            $this->$price = $_price;
-            $this->$image = $_image;
-            $this->$species = $_species;
+            $this->name = $_name;
+            $this->price = $_price;
+            $this->image = $_image;
+            $this->species = $_species;
         }
     }
 
@@ -18,25 +18,25 @@
 
         function __construct(String $_name, Float $_price, String $_image, Species $_species, String $_weight){
             parent::__construct($_name, $_price, $_image, $_species);
-            $this->$weight = $_weight;
+            $this->weight = $_weight;
         }
     }
 
-    class Toys extends Product{
+    class Toy extends Product{
         public $size;
 
         function __construct(String $_name, Float $_price, String $_image, Species $_species, String $_size){
             parent::__construct($_name, $_price, $_image, $_species);
-            $this->$size = $_size;
+            $this->size = $_size;
         }
     }
 
-    class Accessories extends Product{
+    class Accessory extends Product{
         public $use;
 
         function __construct(String $_name, Float $_price, String $_image, Species $_species, String $_use){
             parent::__construct($_name, $_price, $_image, $_species);
-            $this->$use = $_use;
+            $this->use = $_use;
         }
     }
 
@@ -45,16 +45,35 @@
         public $logo;
 
         function __construct(String $_species, String $_logo){
-            $this->$species = $_species;
-            $this->$logo = $_logo;
+            $this->species = $_species;
+            $this->logo = $_logo;
         }
     }
+
+    $species = [
+        'feline' => new Species('Cats', '<i class="fa-solid fa-cat"></i>'),
+        'canine' => new Species('Dogs', '<i class="fa-solid fa-dog"></i>'),
+        'aviate' => new Species('Birds', '<i class="fa-solid fa-crow"></i>'),
+        'acquatics' => new Species('Fishes', '<i class="fa-solid fa-fish-fins"></i>'),
+    ];
+
+    $products = [
+        new Food('Royal Canin Mini Adult', 20, 'https://arcaplanet.vtexassets.com/arquivos/ids/284621/Mini-Adult.jpg?v=638182891693570000', $species['canine'], '10kg'),
+        new Food('Almo Nature Holistic Adult', 20, 'https://arcaplanet.vtexassets.com/arquivos/ids/245173/almo-nature-holistic-cane-adult-medium-pollo-e-riso.jpg', $species['canine'], '10kg'),
+        new Food('Almo Daily Menu Cat', 7.50, 'https://arcaplanet.vtexassets.com/arquivos/ids/245336/almo-daily-menu-cat-400-gr-vitello.jpg', $species['feline'], '0.4kg'),
+        new Food('Guppy in Flakes', 5, 'https://arcaplanet.vtexassets.com/arquivos/ids/272714/tetra-guppy-mini-flakes.jpg', $species['acquatics'], '0.1kg'),
+        new Accessory('Voliera Wilma', 39.50, 'https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg', $species['aviate'], 'Housing'),
+        new Accessory('Tetra Easycristal Water Filters', 30, 'https://arcaplanet.vtexassets.com/arquivos/ids/272741/tetra-easycrystal-filterpack-250-300.jpg', $species['acquatics'], 'Health'),
+        new Toy('Kong Classic', 10, 'https://arcaplanet.vtexassets.com/arquivos/ids/256599/kong-classic1.jpg', $species['canine'], 'Small'),
+        new Toy('Trixie Mouse Peluche', 10, 'https://arcaplanet.vtexassets.com/arquivos/ids/223852/trixie-gatto-gioco-active-mouse-peluche.jpg', $species['feline'], 'Small'),
+    ];
 ?>
 <!DOCTYPE html>
     <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/dcdaab4f9f.js" crossorigin="anonymous"></script>
         <title>Document</title>
     </head>
     <body>
